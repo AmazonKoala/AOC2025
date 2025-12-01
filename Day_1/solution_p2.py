dec43f9b28048ee28308c46dbc0d44d9bfe0e763
Andrew Hyam <andrew@hyam.ca>
2025-12-01 15:13:00 -0500

    Day 1 Finished

---
+++ Day_1/solution_p2.py
@@ -0,0 +1,27 @@
+with open('Day_1/input.txt', 'r') as file:
+    input_list = [line.strip() for line in file]
+
+lock = 50
+zeros = 0
+for command in input_list:
+    if command[0] == 'L':
+        direction = -1
+    else:
+        direction = 1
+
+    dist = int(command[1:])
+
+    while dist > 0:
+        lock += 1 * direction
+
+        if lock < 0:
+            lock += 100
+        if lock > 99:
+            lock -= 100
+    
+        if lock == 0:
+            zeros += 1
+        dist -= 1
+
+print(zeros)
+
